d2524a5b506cb463a0d0c3a114c878f2678121f6
lujiachao <632849093@qq.com>
2018-12-27 15:41:03 +0800

    日志处理更新

--- MiddleWareStudy/Controllers/ValuesController.cs
+++ MiddleWareStudy/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MiddleWareStudy.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
--- MiddleWareStudy/LoggersMiddleware/RequestLogMiddleware.cs
+++ MiddleWareStudy/LoggersMiddleware/RequestLogMiddleware.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +11,7 @@ namespace MiddleWareStudy.LoggersMiddleware
     {
         private readonly RequestDelegate _next; //定义请求委托
         private readonly ILogger _logger; //定义日志
-
+        private StreamReader streamReader;
         public RequestLogMiddleware(RequestDelegate next)
         {
             _next = next;
@@ -21,10 +19,18 @@ namespace MiddleWareStudy.LoggersMiddleware
 
         public async Task Invoke(HttpContext context)
         {
+            streamReader = new StreamReader(context.Request.Body);
+            string RemoteAddress = context.Connection.RemoteIpAddress.ToString();
+            string ForwarderAddress = context.Request.Headers["X-Forwarded-For"];
+            string requestStr = await streamReader.ReadToEndAsync();
             StringBuilder sb = new StringBuilder();
-            sb.Append($"1.{context.Request.Host.ToString()}/n/r");
-            sb.Append($"2.{UriHelper.GetDisplayUrl(context.Request)}/n/r");
-            sb.Append($"3.{context}/n/r");
+            sb.Append($"1.{context.Request.Host.ToString()}/n/r");                 //主机地址
+            sb.Append($"2.{UriHelper.GetDisplayUrl(context.Request)}/n/r");        //请求url
+            sb.Append($"3.{context.TraceIdentifier}/n/r");                         //请求唯一标识
+            sb.Append($"4.Token暂时没有");                                         //token
+            sb.Append($"5.{requestStr}/n/r");                                             //请求body
+            sb.Append($"6.用户id或者标识/n/r");                                    //用户id或者标识
+
         }
     }
 }
