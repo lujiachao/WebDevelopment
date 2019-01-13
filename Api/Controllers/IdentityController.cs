fceebe7c23e099edb359170e74cadf8ea9df6507
ljc <632849093@qq.com>
2019-01-13 18:43:35 +0800

    测试

--- Api/Controllers/IdentityController.cs
+++ Api/Controllers/IdentityController.cs
@@ -11,7 +11,7 @@ namespace Api.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return new JsonResult(from c in User.Claims select new { c.Type, c.Value});
+            return new JsonResult( from c in User.Claims select new { c.Type, c.Value});
         }
     }
 }
